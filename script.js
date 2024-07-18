d14e9056a0d941620d63ae2f52475031e964f490
liyijung <132187351+liyijung@users.noreply.github.com>
2024-07-18 21:19:25 +0800

    version1

--- script.js
+++ script.js
@@ -1391,7 +1391,7 @@ function createInputField(name, value, isVisible) {
                 ${inputField}
             </div>
         `;
-    } else if (['QTY', 'DOC_UNIT_P', 'ST_QTY'].includes(name)) {
+    } else if (['QTY', 'DOC_UNIT_P', 'ST_QTY', 'GOODS_MODEL', 'GOODS_SPEC'].includes(name)) {
         return `
             <div class="form-group ${visibilityClass}" style="width: 60%;">
                 ${inputField}
