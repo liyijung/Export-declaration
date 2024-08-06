3d2e4d233811e2f2d1cff7c3b9b7415ba606c2ec
liyijung <132187351+liyijung@users.noreply.github.com>
2024-08-06 21:34:20 +0800

    version1

--- script.js
+++ script.js
@@ -2009,8 +2009,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 xmlContent += `    <fields>\n      <field_name>${className}</field_name>\n      <field_value>${value}</field_value>\n    </fields>\n`;
             });
 
-            // 固定設置 PER_ST 的值為 1
-            xmlContent += `    <fields>\n      <field_name>PER_ST</field_name>\n      <field_value>1</field_value>\n    </fields>\n`;
+            // 設定 PER_ST 的值
+            let perStValue = (item.querySelector('.ITEM_NO').checked) ? '' : '1';
+            xmlContent += `    <fields>\n      <field_name>PER_ST</field_name>\n      <field_value>${perStValue}</field_value>\n    </fields>\n`;
             
             xmlContent += '  </items>\n';
         });
