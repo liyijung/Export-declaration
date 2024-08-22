e95a3113dd52d6f55b1a5757900b346b1ecca842
liyijung <132187351+liyijung@users.noreply.github.com>
2024-08-23 07:37:48 +0800

    version1

--- script.js
+++ script.js
@@ -1607,6 +1607,7 @@ function addTextarea() {
 function createInputField(name, value, isVisible) {
     const visibilityClass = isVisible ? '' : 'hidden';
     const numberFields = ['QTY', 'DOC_UNIT_P', 'NET_WT', 'ORG_IMP_DCL_NO_ITEM', 'CERT_NO_ITEM', 'ORG_DCL_NO_ITEM', 'EXP_SEQ_NO', 'WIDE', 'LENGT_', 'ST_QTY'];
+    const upperCaseFields = ['LOT_NO', 'SHPR_BONDED_ID', 'CNEE_COUNTRY_CODE', 'TO_CODE', 'DOC_CTN_UM', 'DCL_DOC_TYPE', 'TERMS_SALES', 'CURRENCY', 'DOC_UM', 'ST_MTD', 'ORG_COUNTRY', 'ORG_IMP_DCL_NO', 'BOND_NOTE', 'CERT_NO', 'ORG_DCL_NO', 'EXP_NO', 'WIDE_UM', 'LENGTH_UM', 'ST_UM'];
     const inputType = numberFields.includes(name) ? 'number' : 'text';
     const onInputAttribute = numberFields.includes(name) ? 'oninput="calculateAmount(event); validateNumberInput(event)"' : '';
     const minAttribute = numberFields.includes(name) ? 'min="0"' : '';
@@ -1614,6 +1615,8 @@ function createInputField(name, value, isVisible) {
     const onFocusAttribute = 'onfocus="highlightRow(this)"';
     const onBlurAttribute = 'onblur="removeHighlight(this)"';
     const onKeyDownAttribute = 'onkeydown="handleInputKeyDown(event, this)"';
+    const onInputUpperCaseAttribute = upperCaseFields.includes(name) ? 'oninput="this.value = this.value.toUpperCase()"' : '';
+
 
     // 格式化 ORG_IMP_DCL_NO 和 ORG_DCL_NO 的值
     if (['ORG_IMP_DCL_NO', 'ORG_DCL_NO'].includes(name) && value) {
@@ -1631,7 +1634,7 @@ function createInputField(name, value, isVisible) {
 
     // 處理最大四捨五入至小數6位，並移除後面的多餘零
     const roundedValue = (['QTY', 'DOC_UNIT_P', 'NET_WT', 'WIDE', 'LENGT_', 'ST_QTY'].includes(name) && value) ? parseFloat(value).toFixed(6).replace(/\.?0+$/, '') : escapedValue;
-    const inputField = `<input type="${inputType}" class="${name} ${name === 'CCC_CODE' ? 'CCC_CODE' : 'tax-code-input'}" value="${roundedValue}" ${onInputAttribute} ${minAttribute} ${readonlyAttribute} ${onFocusAttribute} ${onBlurAttribute} ${onKeyDownAttribute} style="flex: 1; margin-right: 0;">`;
+    const inputField = `<input type="${inputType}" class="${name} ${name === 'CCC_CODE' ? 'CCC_CODE' : 'tax-code-input'}" value="${roundedValue}" ${onInputAttribute} ${minAttribute} ${readonlyAttribute} ${onFocusAttribute} ${onBlurAttribute} ${onKeyDownAttribute} ${onInputUpperCaseAttribute} style="flex: 1; margin-right: 0;">`;
 
     if (name === 'NET_WT') {
         return `
