0437e72337a6a6427c8aebb5474ea16bff912e39
liyijung <132187351+liyijung@users.noreply.github.com>
2024-11-06 19:59:53 +0800

    version1

--- exportToPDF.js
+++ exportToPDF.js
@@ -533,10 +533,10 @@ async function exportToPDF() {
 
             // 顯示前置描述和品名
             const descriptionText = [];
-            if (item.sellerItemCode) descriptionText.push(`S/N:${item.sellerItemCode}`);
-            if (item.goodsModel) descriptionText.push(`MODEL:${item.goodsModel}`);
-            if (item.goodsSpec) descriptionText.push(`SPEC:${item.goodsSpec}`);
-            descriptionText.push(item.description); // 添加品名描述
+            if (item.sellerItemCode) descriptionText.push(`S/N:${item.sellerItemCode}`.trim());
+            if (item.goodsModel) descriptionText.push(`MODEL:${item.goodsModel}`.trim());
+            if (item.goodsSpec) descriptionText.push(`SPEC:${item.goodsSpec}`.trim());
+            descriptionText.push(item.description.trim()); // 添加品名描述
 
             if (item.index === '*') {
                 const combinedDescription = descriptionText.join('\n');
