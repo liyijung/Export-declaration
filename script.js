98f107c6fb1e980d37fe59c8b46eace86cf4eb8f
liyijung <132187351+liyijung@users.noreply.github.com>
2024-12-31 22:15:06 +0800

    version1

--- script.js
+++ script.js
@@ -4328,9 +4328,12 @@ function validateDclDocType() {
         // 檢查條件 3：若有 ST_MTD 為 外貨復出口統計方式，則所有項次的 ORG_COUNTRY 不可為空
         if (containsMandatoryOrgCountry && containsMandatoryOrgCountryTW && hasEmptyOrgCountry) {
             rows.forEach(item => {
-                const orgCountryValue = item.querySelector(".ORG_COUNTRY")?.value.trim();
-                if (!orgCountryValue || orgCountryValue.trim() === "") {
-                    setError(item.querySelector(".ORG_COUNTRY"), "生產國別不可為空");
+                const isItemChecked = item.querySelector(".ITEM_NO")?.checked;
+                if (!isItemChecked) {
+                    const orgCountryValue = item.querySelector(".ORG_COUNTRY")?.value.trim();
+                    if (!orgCountryValue || orgCountryValue.trim() === "") {
+                        setError(item.querySelector(".ORG_COUNTRY"), "生產國別不可為空");
+                    }
                 }
             });
             validationErrors.add("國洋貨合併申報，生產國別不可為空（國貨請填 TW ）");
