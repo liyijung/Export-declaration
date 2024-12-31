7b3890c3f9c20d64552c5048832e9dd003a6aded
liyijung <132187351+liyijung@users.noreply.github.com>
2024-12-31 20:37:04 +0800

    version1

--- exportToPDF.js
+++ exportToPDF.js
@@ -240,8 +240,8 @@ async function exportToPDF() {
         doc.text(formattedTotalFobPrice, totalFobPriceX, totalFobPriceY);
 
         // 當旬匯率日期區間
-        const startDate = '1131221'
-        const endDate = '1131231'
+        const startDate = '1140101'
+        const endDate = '1140110'
         
         if (exchangeRate && (Fymd >= startDate && Fymd <= endDate)) {
             // 在 x: 171, y: totalFobPriceTwY 顯示 "TWD"
