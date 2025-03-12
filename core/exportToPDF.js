4b60191710bcc543e0f9edd6b58963ad0013290a
liyijung <132187351+liyijung@users.noreply.github.com>
2025-03-12 09:28:52 +0800

    version1

--- core/exportToPDF.js
+++ core/exportToPDF.js
@@ -134,9 +134,9 @@ async function exportToPDF() {
         if (generalWarehouseChecked) {
             doc.text(`41`, 136, 44) // 運輸方式(一般倉)
             if (dclDocTypeValue === 'F5') {
-                doc.text(`C2038 遠雄自貿港`, 30, 53.5)
+                doc.text(`C2040 遠雄自由貿易港區`, 30, 53.5)
             } else {
-                doc.text(`C2036 遠雄出口貨棧`, 30, 53.5)
+                doc.text(`C2036 遠雄航空出口一般倉`, 30, 53.5)
             }
         } else {
             doc.text(`42`, 136, 44) // 運輸方式(快遞倉)
